efb40e57d5e9169c4e7c50e513362ff0700511bd
Prabesh KC <33727948+kcprabase@users.noreply.github.com>
2018-11-03 08:18:59 +0545

    Added column and item in options

--- multi-column-select/src/app/app.component.ts
+++ multi-column-select/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'multi-column-select';
@@ -15,11 +16,12 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.options = [
-      {id: 1, name:"john", gender: "male", age:20},
-      {id: 2, name:"jina", gender: "female", age:22},
-      {id: 3, name:"tom", gender: "male", age:23},
-      {id: 4, name:"ram", gender: "male", age:24},
-      {id: 5, name:"samy", gender: "female", age:25},
+      {id: 1, name:"john", gender: "male", age:20, "hobby":"football"},
+      {id: 2, name:"jina", gender: "female", age:22, "hobby":"hiking"},
+      {id: 3, name:"tom", gender: "male", age:23, "hobby":"painting"},
+      {id: 4, name:"ram", gender: "male", age:24, "hobby":"hiking"},
+      {id: 5, name:"samy", gender: "female", age:25, "hobby":"coding"},
+      {id: 6, name:"joha", gender: "female", age:26, "hobby":"boating"},
     ]
   }
 
